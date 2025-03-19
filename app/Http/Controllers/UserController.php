96994502f1d33c0b7d81befaa06f0e7b81b908a3
Ferdycebong16 <ferdycebong161@gmail.com>
2025-03-19 19:49:34 +0700

    database model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,22 +2,44 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Kelas;
+use App\Models\UserModel;
 
 class UserController extends Controller
 {
     
-    public function create(){
-        return view('create_user');
-    }
-    
-    public function store(Request $request){
-       
+    public function create()
+    {
+        $kelasModel = new Kelas();
+
+        $kelas = $kelasModel->getKelas();
+
         $data = [
-              'nama' => $request->input('nama'),
-              'kelas' => $request->input('kelas'),
-              'npm' => $request->input('npm'),
+            'title' => 'Create User',
+            'kelas' => $kelas,
         ];
-        return view('profile', $data);
-}
+
+        return view('create_user', $data);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nama' => 'required|string|max:255',
+            'npm' => 'required|string|max:255',
+            'kelas_id' => 'required|exists:kelas,id',
+        ]);
+
+        $user = UserModel::create($validatedData);
+
+        $user->load('kelas');
+
+        return view('profile', [
+            'nama' => $user->nama,
+            'npm' => $user->npm,
+            'nama_kelas' => $user->kelas->nama_kelas ?? 'Kelas tidak ditemukan',
+        ]);
+    }
 }
