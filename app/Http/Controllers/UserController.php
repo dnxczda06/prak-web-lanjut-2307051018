7b1c5f61c738612bce801a4fe44e24cbfdf3d650
Ferdycebong16 <ferdycebong161@gmail.com>
2025-04-15 19:02:49 +0700

    read data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,16 +1,34 @@
 <?php
 
 namespace App\Http\Controllers;
-
-
-use Illuminate\Http\Request;
 use App\Models\Kelas;
 use App\Models\UserModel;
+use App\Models\Models;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public $kelasModel;
+    public $userModel;
     
-    public function create()
+    public function __construct() {
+        $this->userModel = new UserModel();
+        $this->kelasModel = new Kelas();
+
+    }
+
+     public function index()
+     {
+        $data = [
+        'title' => 'Create User',
+        'users' => $this->userModel->getUser(),
+        ];
+        
+        return view('list_user', $data);
+    }
+
+    public function create(){
+
     {
         $kelasModel = new Kelas();
 
@@ -22,24 +40,47 @@ class UserController extends Controller
         ];
 
         return view('create_user', $data);
-    }
 
-    public function store(Request $request)
-    {
+    }
+}
+
+
+    public function store(Request $request){
+
+        $this->userModel->create([
+            'nama' => $request->input('nama'),
+            'npm' => $request->input('npm'),
+            'kelas_id' => $request->input('kelas_id'),
+            ]);
+
+            return redirect()->to('/user');
+
+
         $validatedData = $request->validate([
             'nama' => 'required|string|max:255',
             'npm' => 'required|string|max:255',
             'kelas_id' => 'required|exists:kelas,id',
         ]);
-
+    
         $user = UserModel::create($validatedData);
-
+    
         $user->load('kelas');
-
+    
         return view('profile', [
             'nama' => $user->nama,
             'npm' => $user->npm,
             'nama_kelas' => $user->kelas->nama_kelas ?? 'Kelas tidak ditemukan',
         ]);
+
+
+
+       $data = [
+
+        'nama' => $request->input('nama'),
+        'kelas' => $request->input('kelas'),
+        'npm' => $request->input('npm'),
+
+       ];
+       return view('profile', $data);
     }
 }
